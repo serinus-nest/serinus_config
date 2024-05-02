4c342f1cf807a305b29b71b80dd49720046003f0
Francesco Vallone <vallonefrancesco587@gmail.com>
2024-05-02 23:56:35 +0200

    feat: first commit

---
+++ example/lib/app_provider.dart
@@ -0,0 +1,13 @@
+import 'package:serinus/serinus.dart';
+import 'package:serinus_config/serinus_config.dart';
+
+class AppProvider extends Provider {
+
+  final ConfigService _configService;
+
+  AppProvider(this._configService);
+
+  String sendHelloWorld() {
+    return _configService.getOrThrow('TEST');
+  }
+}
